7cd3a8aebe9b4f6bca1b9e781d31ad2361e21f85
zeolla <azeolla@alumno.unlam.edu.ar>
2021-04-12 21:28:19 -0300

    primer commit

---
+++ src/proyecto/Calculadora.java
@@ -0,0 +1,32 @@
+package proyecto;
+import java.util.Scanner;
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Integer a;
+		Integer b;
+		Integer opcion;
+		Integer resultado;
+		do{
+		System.out.println("Ingrese su operación /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
+		
+		Scanner teclado=new Scanner(System.in);
+		opcion = teclado.nextInt();
+		}while(opcion < 1 && opcion > 4);
+		System.out.println("Ingrese el primer numero");
+		a = teclado.nextInt();
+		System.out.println("Ingrese el segundo numero");
+		a = teclado.nextInt();
+		if(opcion.equals(1)){
+		resultado = a + b;
+		}else if(opcion.equals(2)){
+		resultado = a - b;
+		}else if(opcion.equals(3)){
+		resultado = a * b;
+		}else{
+		resultado = a / b;
+		}
+		System.out.println("El resultado es " + resultado.toString());
+		}
+	
+}
